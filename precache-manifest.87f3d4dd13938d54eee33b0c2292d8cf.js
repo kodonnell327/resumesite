c30b422ee4144c34cdb751d55d0b0f1b9eb4b56a
Kaitlyn O'Donnell <kodonnell327@gmail.com>
2019-08-07 22:36:05 -0400

    Updates

--- precache-manifest.87f3d4dd13938d54eee33b0c2292d8cf.js
+++ precache-manifest.87f3d4dd13938d54eee33b0c2292d8cf.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3a7051caf9df4cd3aa65306b9f708548",
+    "revision": "cfad40268f6af363eab3fffaa88c3ed6",
     "url": "/index.html"
   },
   {
-    "revision": "4b623e8593c9ac990379",
+    "revision": "f6b72dc922d43f2ccce8",
     "url": "/static/css/main.2cce8147.chunk.css"
   },
   {
-    "revision": "2e0129063922e3bb197a",
-    "url": "/static/js/2.b41502e9.chunk.js"
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
   },
   {
-    "revision": "4b623e8593c9ac990379",
-    "url": "/static/js/main.28647029.chunk.js"
+    "revision": "f6b72dc922d43f2ccce8",
+    "url": "/static/js/main.787f66df.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
