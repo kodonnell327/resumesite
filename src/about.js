71a5991cee0b6f35fca681ccf041b9bc653f1a23
Kaitlyn O'Donnell <kodonnell327@gmail.com>
2019-10-15 19:04:11 -0400

    WIP menu styling and structuring

--- src/about.js
+++ src/about.js
@@ -8,10 +8,43 @@ import './scss/global.scss';
 class About extends React.Component {
     render() {
         return (
-            <div>
+            <div className="about-page">
                 <Menu></Menu>
-                <h1>About Me!</h1>
-                <p></p>
+                <div className="left-side-bar">
+                    <h1>About Me</h1>
+                    <p>My name is Kaitlyn O'Donnell and I am a software developer located in Boston, MA. I am graduating from Northeastern University in the spring of 2020.</p>
+                    <p>My software interests include:
+                        <ul>
+                            <li>User centered design</li>
+                            <li>Writing understandable code</li>
+                            <li>Web development (full stack)</li>
+                            <li>Algorithmic fairness</li>
+                        </ul>
+                    </p>
+                    <p>
+                        My non-software interests include:
+                        <ul>
+                            <li>Woodworking - specifically fretwork with scrollsaws and pen turning</li>
+                            <li>Crochet - usually hats</li>
+                            <li>Piano</li>
+                            <li>Swimming</li>
+                        </ul>
+                    </p>
+
+                    <p>
+                        My favorite non-STEM classes I took in college were:
+                        <ul>
+                            <li>MSCR 1220 - Media, Culture, and Society</li>
+                            <li>SOCL 1280 - 21st Century Workplace</li>
+                            <li>MUSC 1119 - Fundamentals of Western Music Theory</li>
+                            <li>ENGW 3302 - Advanced Writing in the Technical Professions</li>
+                            <li>THTR 1170 - The Eloquent Presenter</li>
+                        </ul>
+                    </p>
+                </div>
+                <div className="right-side-bar">
+
+                </div>
             </div>
             
         );
--- src/menu.js
+++ src/menu.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import './scss/menu.scss';
+import capitalize from './utils/capitalize';
 
-function Menu() {
-    return (
-        <header className="menu-header">
-            <ul className="menu-list">
-                <li className='menu-list-item'>
-                    <Link to='/' className="menu-link">
-                    Home</Link>
-                </li>
-                <li className="menu-list-item">
-                <Link to='/about' className="menu-link">
-                    About</Link>
-                </li>
-            </ul>
-        </header>
-    );
+class Menu extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPage: props.currentPage,
+        };
+    }
+
+    getListItem(link, title) {
+        let classes = ['menu-link'];
+        if (this.state.currentPage === title) {
+            classes.push('currentPage');
+        }
+        return (
+            <li className='menu-list-item'>
+                <Link to={link} className={classes.join(' ')}>
+                        {capitalize(title)}</Link>
+            </li>
+        );
+    }
+
+    render() {
+        return (
+            <header className="menu-header">
+                <ul className="menu-list">
+                    {this.getListItem('/', 'home')}
+                    {this.getListItem('/about', 'about')}
+                </ul>
+            </header>
+        );
+    }
 }
 
 export default Menu;
